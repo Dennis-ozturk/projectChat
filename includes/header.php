b7573ea8e56f7df2699f413338b5ab78aa26923c
Dennis Ozturk <dennis.dada@hotmail.se>
2017-12-19 23:29:28 +0100

    g

--- includes/header.php
+++ includes/header.php
@@ -21,12 +21,42 @@ session_start();
     </head>
     <body>
 
+  <nav class="navbar navbar-toggleable-md navbar-dark bg-faded">
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="#">Navbar</a>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Dropdown link
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <a class="dropdown-item" href="#">Something else here</a>
+          </div>
+      </li>
+    </ul>
+  </div>
+</nav>
+
 <header class="jumbotron">
       <div class="container">
           <div class="row row-header">
               <div class="jumbo-head">
-                <img src="img/techknight.png" style="float:left; width:120px; height:120px;">
-                  <h1 style="float:left; padding-left: 30px;padding-top:40px;">TechKnight</h1>
+                <img onclick="homePage()" src="img/techknight.png" style="float:left; width:120px; height:120px;">
+                  <h1 onclick="homePage()" style="float:left; padding-left: 30px;padding-top:40px;">TechKnight</h1>
                   <?php if(isset($_SESSION['name'])) { ?>
                     <a href='./logout.php'>Logout</a>
                     <a href='./home.php'>Home</a>
