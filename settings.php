24587174c88acfabff77747013072df7c787dbfb
Dennis Ozturk <dennis.dada@hotmail.se>
2018-02-06 10:45:14 +0100

    Edited settings

--- settings.php
+++ settings.php
@@ -16,16 +16,16 @@ if (isset($_SESSION['name'])){
 <br>
 <span> Enter old password: </span>
 <br>
-<input type="password" name="newPassword">
+<input type="password" name="newPassword1">
 <br>
 <span> Enter old password: </span>
 <br>
-<input type="password" name="newPassword">
+<input type="password" name="newPassword2">
 <br>
 <br>
 <input type="submit" name="subSettings" value="Confirm">
 </form>
-</div>
+</div>	
 <?php 
 }else {
 ?>
@@ -34,6 +34,32 @@ if (isset($_SESSION['name'])){
 
 <?php 
 }
+?>
+
+<?php 
+if(isset($_POST['subSettings'])){
+	$oldPassword = mysqli_real_escape_string($con, $_SESSION['oldPassword']);
+	$newPassword1 = mysqli_real_escape_string($con, $_SESSION['newPassword1']);
+	$newPassword2 = mysqli_real_escape_string($con, $_SESSION['newPassword2']);
+	$sqlEmail = "SELECT email FROM users WHERE email='$email' ";
+
+	$currentPassword = $con->query("SELECT password");
+
+	$encrypt = md5($oldPassword);
+
+	$result = mysqli_query($con, $sqlEmail);
+
+	if(mysqli_num_rows($result)){
+		if
+	}else{
+
+	}
+
+
+
+
+}
+
 ?>
 
 <?php
