6fb7c9cd2f853818832197393571f209c71cab57
Dennis Ozturk <dennis.dada@hotmail.se>
2018-02-07 13:55:19 +0100

    Change password works now, checks if same password or not

--- settings.php
+++ settings.php
@@ -5,26 +5,26 @@ include 'includes/header.php';
 if (isset($_SESSION['name'])){
 ?>
 <div class="settings">
-<h2>Settings</h2>
-<span>Current email: <?php echo $_SESSION['email']; ?> </span>
-<br>
-<form method="POST" enctype="multipart/form-data">
-<h4 style="text-align: left;">Change Password:</h4>
-<span> Enter old password: </span>
-<br>
-<input type="password" name="oldPassword">
-<br>
-<span> Enter new password: </span>
-<br>
-<input type="password" name="newPassword1">
-<br>
-<span> Enter new password: </span>
-<br>
-<input type="password" name="newPassword2">
-<br>
-<br>
-<input type="submit" name="subSettings" value="Confirm">
-</form>
+	<h2>Settings</h2>
+	<span>Current email: <?php echo $_SESSION['email']; ?> </span>
+	<br>
+	<form method="POST" enctype="multipart/form-data">
+		<h4 style="text-align: left;">Change Password:</h4>
+		<span> Enter old password: </span>
+		<br>
+		<input type="password" name="oldPassword">
+		<br>
+		<span> Enter new password: </span>
+		<br>
+		<input type="password" name="newPassword1">
+		<br>
+		<span> Enter new password: </span>
+		<br>
+		<input type="password" name="newPassword2">
+		<br>
+		<br>
+		<input type="submit" name="subSettings" value="Confirm">
+	</form>
 </div>
 <?php
 }else {
@@ -47,15 +47,23 @@ if(isset($_POST['subSettings'])){
 
 	$currentPassword = $con->query("SELECT password FROM users WHERE email='$email'");
 
-	$encrypt = md5($oldPassword);
+	$encryptOld = md5($oldPassword);
 
 	$result = mysqli_query($con, $sqlEmail);
 
 
 	if (mysqli_num_rows($result) == 1) {
 		while ($row = $currentPassword->fetch_assoc()) {
-			if ($row['password'] == $encrypt) {
-				echo "Correct";
+			if ($row['password'] == $encryptOld) {
+				$encryptNew = md5($newPassword1);
+				$sqlUpdate = "UPDATE users SET password='$encryptNew' WHERE email='$email'";
+				if (mysqli_query($con, $sqlUpdate)) {
+					echo "Updated";
+				}else {
+					echo "something went wrong";
+				}
+			}else {
+				echo "Something went wrong";
 			}
 		}
 	}
