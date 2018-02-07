17b19cf1ffe1fefe8338897d4dd8ee789f24e6a1
Dennis Ozturk <dennis.dada@hotmail.se>
2018-02-07 12:25:09 +0100

    Fixed settings error

--- settings.php
+++ settings.php
@@ -1,7 +1,7 @@
 <?php
 include 'includes/header.php';
 ?>
-<?php 
+<?php
 if (isset($_SESSION['name'])){
 ?>
 <div class="settings">
@@ -14,49 +14,51 @@ if (isset($_SESSION['name'])){
 <br>
 <input type="password" name="oldPassword">
 <br>
-<span> Enter old password: </span>
+<span> Enter new password: </span>
 <br>
 <input type="password" name="newPassword1">
 <br>
-<span> Enter old password: </span>
+<span> Enter new password: </span>
 <br>
 <input type="password" name="newPassword2">
 <br>
 <br>
 <input type="submit" name="subSettings" value="Confirm">
 </form>
-</div>	
-<?php 
+</div>
+<?php
 }else {
 ?>
 
 <h1>Login please</h1>
 
-<?php 
+<?php
 }
 ?>
 
-<?php 
+<?php
 if(isset($_POST['subSettings'])){
-	$oldPassword = mysqli_real_escape_string($con, $_SESSION['oldPassword']);
-	$newPassword1 = mysqli_real_escape_string($con, $_SESSION['newPassword1']);
-	$newPassword2 = mysqli_real_escape_string($con, $_SESSION['newPassword2']);
+	$oldPassword = mysqli_real_escape_string($con, $_POST['oldPassword']);
+	$newPassword1 = mysqli_real_escape_string($con, $_POST['newPassword1']);
+	$newPassword2 = mysqli_real_escape_string($con, $_POST['newPassword2']);
+	$email = $_SESSION['email'];
 	$sqlEmail = "SELECT email FROM users WHERE email='$email' ";
 
-	$currentPassword = $con->query("SELECT password");
+
+	$currentPassword = $con->query("SELECT password FROM users WHERE email='$email'");
 
 	$encrypt = md5($oldPassword);
 
 	$result = mysqli_query($con, $sqlEmail);
 
-	if(mysqli_num_rows($result)){
-		if
-	}else{
 
+	if (mysqli_num_rows($result) == 1) {
+		while ($row = $currentPassword->fetch_assoc()) {
+			if ($row['password'] == $encrypt) {
+				echo "Correct";
+			}
+		}
 	}
-
-
-
 
 }
 
