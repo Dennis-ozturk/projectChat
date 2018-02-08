d487ffac0934b5ee73f3e04a5c4b595fd00fff1e
Dennis Ozturk <dennis.dada@hotmail.se>
2018-02-08 11:42:59 +0100

    Added profile page, removed bootstrap

--- index.php
+++ index.php
@@ -2,38 +2,4 @@
 include 'includes/header.php';
 ?>
 
-<div class="container">
-        <div class="row row-content align-items-center">
-            <div class="col-sm-4 col-md-3 flex-last">
-                <h3>Services</h3>
-            </div>
-            <div class="col-sm cal-md flex-first">
-                <h2>Sub-header</h2>
-                <p>A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.</p>
-            </div>
-        </div>
-
-
-        <div class="row row-content align-items-center">
-            <div class="col-sm-4 col-md-3">
-                <h3>Personal</h3>
-            </div>
-            <div class="col-sm cal-md">
-                <h2>Sub-header</h2>
-                <p>Featuring mouthwatering combinations with a choice of five different salads, six enticing appetizers, six main entrees and five choicest desserts. Free flowing bubbly and soft drinks. All for just $19.99 per person </p>
-            </div>
-        </div>
-
-        <div class="row row-content align-items-center">
-            <div class="col-sm-4 col-md-3 flex-last">
-                <h3>Location</h3>
-            </div>
-            <div class="col-sm col-md flex-first">
-                <h2>Sub-header</h2>
-                <p>Award winning three-star Michelin chef with wide International experience having worked closely with whos-who in the culinary world, he specializes in creating mouthwatering Indo-Italian fusion experiences. </p>
-            </div>
-        </div>
-    </div>
-
-
 <?php include 'includes/footer.php'; ?>
---
+++ profile.php
@@ -0,0 +1,27 @@
+<?php include 'includes/header.php'; ?>
+<?php
+
+$email = $_SESSION['email'];
+
+if (isset($_SESSION['name'])) {
+  $profile = "SELECT name, email, password, file, type, size, date_added FROM users WHERE email='$email'";
+  $resultProfile = mysqli_query($con, $profile);
+
+  if (mysqli_num_rows($resultProfile)) {
+    while ($row = $resultProfile->fetch_assoc()) {
+      echo "<div class=" . 'profile' .">";
+        echo "<div class=" . 'profile-section' .">";
+          echo "<img class=" . 'profile-image' . " src=" . 'uploads/profile/' . $row['file'] .">";
+          echo "<h4> Username: ". $row['name'] ."</h4>";
+          echo "<h4> Email: ". $row['email'] ."</h4>";
+        echo "</div>";
+      echo "</div>";
+    }
+  }
+}
+
+
+?>
+
+
+<?php include 'includes/footer.php'; ?>
